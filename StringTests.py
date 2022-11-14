28e24a5f63cffa04c713d3a69ab12baaf3e35d29
Charles Neukuckatz <charneuk@gmail.com>
2022-11-14 16:38:06 -0600

    Remove1standLastLetterofString

--- StringTests.py
+++ StringTests.py
@@ -1,2 +1,6 @@
-string = "ddkshjald"
-print (string[1] + (string[8]))
+def FirstAndLast(string):
+    aos = string.split();
+    res = "";
+    for a in aos : 
+        res += a[1:len(a) -1];
+    return res;
