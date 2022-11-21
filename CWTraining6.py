61cecb18a88a6b3cdfd66f8f2ea40a33a88d2e2a
Charles Neukuckatz <charneuk@gmail.com>
2022-11-20 21:49:48 -0600

    RepeatStringQuestion

---
+++ CWTraining6.py
@@ -0,0 +1,10 @@
+#Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+#Examples (input -> output)
+#6, "I"     -> "IIIIII"
+#5, "Hello" -> "HelloHelloHelloHelloHello"
+
+def repeat_str(repeat, string):
+    for i in range(repeat):
+        for i in string:
+            return (repeat * string)
